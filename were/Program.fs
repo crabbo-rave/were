1ba1a6d92dfebc0f03ef0a3527f9b77382c00a35
static-binary <vatisprocurator@gmail.com>
2022-01-10 09:12:52 -0500

    switched to the Argu lib

--- were/Program.fs
+++ were/Program.fs
@@ -3,5 +3,5 @@ open WereMethods
 
 [<EntryPoint>]
 let main args =
-    printfn "%A" (executeAction (parseArgs (Array.toList args)))
+    printfn "%A" usage
     0
--- were/WereMethods.fs
+++ were/WereMethods.fs
@@ -4,58 +4,53 @@ open ANSIConsole
 open System
 open System.Text.RegularExpressions
 open System.IO
+open Argu
+open HelperMethods
 
-if not (ANSIInitializer.Init false) then ANSIInitializer.Enabled <- false;
+type Arguments =
+    | [<Mandatory>] FileName of filname:string
+    | Count of expr:string
+    | Delete of expr:string
+    | Replace of expr:string * replacement:string
+    | Path of path:string
 
-type Arguments = 
-    {
-        filename: string
-        action: string
-        word: string
-        case: bool
-        path: string option
-    }
+    interface IArgParserTemplate with
+        member s.Usage = 
+            match s with
+            | FileName _ -> "specify a text file"
+            | Count _ -> "specify an expression"
+            | Delete _ -> "specify an expression"
+            | Replace _ -> "specify an expression and a replacement"
+            | Path _ -> "specify a working directory to place the output file"
 
-let printUsage = 
-    eprintfn "%A - count, delete, or replace all occurences of a specific string in a text file" ("were".Color(ConsoleColor.Green))
-    eprintfn "Usage: %A <filename> <action> <word> <case> <output-path>" ("were".Color(ConsoleColor.Yellow))
-    eprintfn "%A: the name of the text file you want %A to process" ("<filename>".Color(ConsoleColor.Green)) ("were".Color(ConsoleColor.Yellow))
-    eprintfn """%A choices:
-           'count' to count all occurences
-           'delete' to delete all occurences
-           'replace' to replace all occurences with something else""" ("<action>".Color(ConsoleColor.Green))
-    eprintfn "%A: the word or regular expression that %A will use" ("<word>".Color(ConsoleColor.Green)) ("were".Color(ConsoleColor.Yellow))
-    eprintfn """%A: tells %A whether to be case-insensitive or match the case of you word.
-             choices are 'false' for case-insensitive, and 'true' for the opposite.""" ("<case>".Color(ConsoleColor.Green)) ("were".Color(ConsoleColor.Yellow))
-    eprintfn "%A: The path to the directory of the output file or an existing text file only for the %A and %A actions" ("<case>".Color(ConsoleColor.Green))
+let parser = ArgumentParser.Create<Arguments>(programName = "were.exe")
 
-let parseArgs (args: list<string>): Arguments =
-    if args.Length <> 4 then
-        printUsage
-        failwithf "%A wrong number of arguments, expected 4" ("Error:".Color(ConsoleColor.Red))
-    {
-        filename = args[0]
-        action = args[1]
-        word = args[2]
-        case = bool.Parse args[3]
-        path = Some args[4] 
-    }
+let usage = parser.PrintUsage()
 
-let executeAction (args: Arguments) =
-    let regexActions (exprToMatch: Regex) (input : string) (action: string) =
-        match action with
-        | "count" -> $"{exprToMatch.Matches(input).Count} occurences of the expression {exprToMatch}"
-        | "delete" -> 
-            
-        
-    let deleteAll exprToMatch (input: string) =
-        Regex.Replace()
-    let rgxExpr = Regex args.word
-    let fileContents = File.ReadAllText args.filename
-    match args.action.ToLower() with
-    | "count" ->
-        $"{countMatches rgxExpr fileContents} occurences of the expression {rgxExpr}"
-    | "delete" -> ""
-    | _ -> ""
+let parseArgs args =
+    let results = parser.Parse (args)
+    results.GetAllResults()
 
-        
+let exeuteActions (results: ParseResults<Arguments>) =
+    let data = File.ReadAllText (results.GetResult FileName)
+    let determineAction =
+        if (containsMoreThan results [Delete; Count; Replace] 2) then
+            failwith "Cannot use --delete, --count, or --replace together. Choose one."
+        elif noneOfElements results [Delete; Count; Replace] then   
+            failwith "At most one of --delete, --count, or --replace is mandatory"
+        else    
+            findItem
+        //match results with
+        //| Delete when results.Contains Delete -> Delete
+        //| Count when result.Contains Count -> Count
+        //| Replace when results.Contains Replace -> Replace
+
+    let rgxExpr = Regex (results.GetResult Expr)
+
+    let writeToFile (path: string) (data: string) =
+        use FileStream fs = File.Create(path) 
+        let info = UTF8Encoding(true).GetBytes(data)
+        fs.Write(info, 0, info.Length)
+
+    let actionReturn =
+        ()
