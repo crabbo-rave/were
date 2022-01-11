4990a8daedd696c76b097ce5676954e7f8408612
static-binary <vatisprocurator@gmail.com>
2022-01-10 19:24:53 -0500

    fixing bugs

--- were/WereMethods.fs
+++ were/WereMethods.fs
@@ -34,20 +34,43 @@ let parseArgs args =
 let exeuteActions (results: ParseResults<Arguments>) =
     let data = File.ReadAllText (results.GetResult FileName)
 
+    let writeToFile (path: string) (data: string) =
+        try 
+            use FileStream fs = File.Create(path) 
+            let info = UTF8Encoding(true).GetBytes(data)
+            fs.WriteAllBytes(info, 0, info.Length)
+        with 
+            | :? IOException -> failwith "Error writing to file"
+    
     let determineAction =
         if containsMoreThan results [Delete; Count; Replace] 2 then
             failwith "Cannot use --delete, --count, or --replace together. Choose one."
         elif noneOfElements results [Delete; Count; Replace] then   
             failwith "At most one of --delete, --count, or --replace is mandatory"
         else    
-            findItem
+            //findItem
+            Delete
 
-    let rgxExpr = Regex (results.GetResult Expr)
+    let rgxExpr = Regex (results.GetResult (determineAction))
+    
+    let doAction (action: Arguments) (input: string) (expr: Regex) =
+        match action with
+        | Count ->
+            expr.Matches(input).Count
+        | Delete ->
+            writeToFile (results.GetResult Path) (expr.Replace(input, ""))
+        | Replace ->
+            writeToFile (results.GetResult Path) (expr.Replace(input, ((results.GetResult Replace).snd)))
+            
+    let actionReturn (action: Arguments) (expr: Regex) =
+        match action with
+        | Count ->
+            $"{doAction Count data expr} occurences of the expression {results.GetResult expr}"
+        | Delete ->
+            doAction Delete data expr
+            $"Succesfully wrote to file {results.GetResult Path}"
+        | Replace ->
+            doAction Replace data expr
+            $"Succesfully wrote to file {results.GetResult Path}"
 
-    let writeToFile (path: string) (data: string) =
-        use FileStream fs = File.Create(path) 
-        let info = UTF8Encoding(true).GetBytes(data)
-        fs.WriteAllBytes(info, 0, info.Length)
-
-    let actionReturn =
-        ()
+    actionReturn
